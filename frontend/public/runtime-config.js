69c7d922f9088803e5c2cec53c5be3cd657f472e
Albert <albertaguileraperez@gmail.com>
2026-07-03 11:32:21 +0200

    feat(adsense): add first adsense block

--- frontend/public/runtime-config.js
+++ frontend/public/runtime-config.js
@@ -1,4 +1,4 @@
 globalThis.commanderZoneRuntimeConfig = Object.freeze({
   "googleClientId": "656749535505-qgai9ul27i0iju4hb3nn22cdmope5ckq.apps.googleusercontent.com",
-  "googleAdsenseClient": ""
+  "googleAdsenseClient": "ca-pub-8530144714199347"
 });
